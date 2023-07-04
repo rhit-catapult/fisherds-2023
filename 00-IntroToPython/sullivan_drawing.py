9cd331479038e07353c4fc342d7e9f9974e70706
David Fisher <fisherds@rose-hulman.edu>
2023-07-04 14:57:40 -0500

    added cheats

--- 00-IntroToPython/sullivan_drawing.py
+++ 00-IntroToPython/sullivan_drawing.py
@@ -10,6 +10,9 @@ def main():
     screen = pygame.display.set_mode((screen_size, screen_size))
     pygame.display.set_caption("Drawing!")
 
+    mini = 1
+    max = 8573
+    is_pressed = False
     num = 0
     clock = pygame.time.Clock()
     big_text = ''
@@ -22,12 +25,20 @@ def main():
             if event.type == pygame.KEYDOWN:
                 keys_pressed = pygame.key.get_pressed()
                 if keys_pressed[pygame.K_SPACE]:
-                    num = random.randint(1, 8573)
-                    # print(num)
+                    is_pressed = True
+                if keys_pressed[pygame.K_r]:
+                    is_pressed = False
+                    mini = 1
+                    max = 8573
+                if keys_pressed[pygame.K_c]:
+                    mini = 1262
+                    max = 1364
+
             if event.type == pygame.QUIT:
                 sys.exit()
 
-
+        if is_pressed == False:
+            num = random.randint(mini, max)
         screen.fill((255, 255, 255))
         screen.blit(background, (0, 0))
         pygame.draw.rect(screen, (254, 230, 70), (50, 300, 400, 100))
